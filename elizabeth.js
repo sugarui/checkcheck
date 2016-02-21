db9b4b6aed63c4b5001c3ce756f639d781841d4d
SeungHye Lee i <sugarui@naver.com>
2016-02-21 22:23:10 +0900

    파일 분리: elizabeth_call에서 elizabeth를 불러오도록 함. 북마클릿코드 간결화 및 주소를 유지한채로 유지보수하기 위함.

--- elizabeth.js
+++ elizabeth.js
@@ -4,12 +4,12 @@ frame.style.zIndex = 999999;
 frame.style.top = '0px';
 frame.style.right = '0px';
 frame.style.position = 'fixed';
-frame.style.width = '240px';
+frame.style.width = '300px';
 frame.style.padding = '10px';
-frame.style.background = '#ffff00';
+frame.style.background = '#f0f0f0';
 
-var guide = document.createElement('label');
-guide.innerHTML = 'title';
+var guide = document.createElement('p');
+guide.innerHTML = '제목이나 저자';
 guide.style.margin ='0 6px 0 0';
 
 var inputBox = document.createElement('input');
@@ -20,7 +20,7 @@ inputBox.style.margin = '0 6px 0 0';
 
 var button = document.createElement('input');
 button.setAttribute('type','button');
-button.setAttribute('value','find');
+button.setAttribute('value','찾아줘');
 
 frame.appendChild(guide);
 frame.appendChild(inputBox);
@@ -28,8 +28,8 @@ frame.appendChild(button);
 
 button.addEventListener('click', function(){
 	var queryName = document.getElementById('inp').value;
-	var url_ridi = 'http://ridibooks.com/search/?q='+ queryName;
-	var url_usb = 'http://userstorybook.net/books/?keyword='+ queryName;
+	var url_ridi = 'http://ridibooks.com/search/?q='+queryName;
+	var url_usb = 'http://userstorybook.net/books/?keyword='+queryName;
 	window.open(url_ridi);
 	window.open(url_usb);
 	document.getElementsByTagName('body')[0].removeChild(frame);
---
+++ elizabeth_call.js
@@ -0,0 +1,6 @@
+(function(){
+var sc = document.createElement('script');
+sc.setAttribute('type','text/javascript');
+sc.setAttribute('src','http://sugarui.net/program/elizabeth/elizabeth.js');
+document.body.appendChild(sc);
+})();
