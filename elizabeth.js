149deee6b929db6f7c8aaba0ae22670802811acf
SeungHye Lee i <sugarui@naver.com>
2016-02-22 01:57:42 +0900

    기능추가: 닫기버튼 추가 / 기타: 변수이름 변경, 한줄코드 주석으로 추가

--- elizabeth.js
+++ elizabeth.js
@@ -4,7 +4,7 @@ frame.style.zIndex = 999999;
 frame.style.top = '0px';
 frame.style.right = '0px';
 frame.style.position = 'fixed';
-frame.style.width = '300px';
+frame.style.width = '400px';
 frame.style.padding = '10px';
 frame.style.background = '#f0f0f0';
 
@@ -12,27 +12,44 @@ var guide = document.createElement('p');
 guide.innerHTML = '제목이나 저자';
 guide.style.margin ='0 6px 0 0';
 
-var inputBox = document.createElement('input');
-inputBox.setAttribute('type','text');
-inputBox.setAttribute('size','20');
-inputBox.id = 'inp';
-inputBox.style.margin = '0 6px 0 0';
+var input_box = document.createElement('input');
+input_box.setAttribute('type','text');
+input_box.setAttribute('size','15');
+input_box.id = 'inp';
+input_box.style.margin = '0 6px 0 0';
 
-var button = document.createElement('input');
-button.setAttribute('type','button');
-button.setAttribute('value','찾아줘');
+var button_find = document.createElement('input');
+button_find.setAttribute('type','button');
+button_find.setAttribute('value','찾아줘');
+
+var button_close = document.createElement('input');
+button_close.setAttribute('type','button');
+button_close.setAttribute('value','닫기');
 
 frame.appendChild(guide);
-frame.appendChild(inputBox);
-frame.appendChild(button);
+frame.appendChild(input_box);
+frame.appendChild(button_find);
+frame.appendChild(button_close);
 
-button.addEventListener('click', function(){
+button_find.addEventListener('click', function(){
 	var queryName = document.getElementById('inp').value;
 	var url_ridi = 'http://ridibooks.com/search/?q='+queryName;
 	var url_usb = 'http://userstorybook.net/books/?keyword='+queryName;
 	window.open(url_ridi);
 	window.open(url_usb);
 	document.getElementsByTagName('body')[0].removeChild(frame);
-})
+});
+
+button_close.addEventListener('click', function(){
+	document.getElementsByTagName('body')[0].removeChild(frame);
+});
 
 document.getElementsByTagName('body')[0].appendChild(frame);
+
+/*
+SINGLE LINE CONVERTING RESULT 
+using http://mrcoles.com/bookmarklet/
+
+javascript:(function()%7Bvar%20frame%20%3D%20document.createElement('div')%3Bframe.style.zIndex%20%3D%20999999%3Bframe.style.top%20%3D%20'0px'%3Bframe.style.right%20%3D%20'0px'%3Bframe.style.position%20%3D%20'fixed'%3Bframe.style.width%20%3D%20'400px'%3Bframe.style.padding%20%3D%20'10px'%3Bframe.style.background%20%3D%20'%23f0f0f0'%3Bvar%20guide%20%3D%20document.createElement('p')%3Bguide.innerHTML%20%3D%20'%EC%A0%9C%EB%AA%A9%EC%9D%B4%EB%82%98%20%EC%A0%80%EC%9E%90'%3Bguide.style.margin%20%3D'0%206px%200%200'%3Bvar%20input_box%20%3D%20document.createElement('input')%3Binput_box.setAttribute('type'%2C'text')%3Binput_box.setAttribute('size'%2C'15')%3Binput_box.id%20%3D%20'inp'%3Binput_box.style.margin%20%3D%20'0%206px%200%200'%3Bvar%20button_find%20%3D%20document.createElement('input')%3Bbutton_find.setAttribute('type'%2C'button')%3Bbutton_find.setAttribute('value'%2C'%EC%B0%BE%EC%95%84%EC%A4%98')%3Bvar%20button_close%20%3D%20document.createElement('input')%3Bbutton_close.setAttribute('type'%2C'button')%3Bbutton_close.setAttribute('value'%2C'%EB%8B%AB%EA%B8%B0')%3Bframe.appendChild(guide)%3Bframe.appendChild(input_box)%3Bframe.appendChild(button_find)%3Bframe.appendChild(button_close)%3Bbutton_find.addEventListener('click'%2C%20function()%7Bvar%20queryName%20%3D%20document.getElementById('inp').value%3Bvar%20url_ridi%20%3D%20'http%3A%2F%2Fridibooks.com%2Fsearch%2F%3Fq%3D'%2BqueryName%3Bvar%20url_usb%20%3D%20'http%3A%2F%2Fuserstorybook.net%2Fbooks%2F%3Fkeyword%3D'%2BqueryName%3Bwindow.open(url_ridi)%3Bwindow.open(url_usb)%3Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bbutton_close.addEventListener('click'%2C%20function()%7Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bdocument.getElementsByTagName('body')%5B0%5D.appendChild(frame)%7D)()
+
+*/
