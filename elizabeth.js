784d3068f0be2f6e4d140d7f70240b1dd3db9b08
SeungHye Lee i <sugarui@naver.com>
2016-02-22 14:08:19 +0900

    기능추가: 알라딘 검색 추

--- elizabeth.js
+++ elizabeth.js
@@ -32,9 +32,19 @@ frame.appendChild(button_find);
 frame.appendChild(button_close);
 
 button_find.addEventListener('click', function(){
-	var queryName = document.getElementById('inp').value;
-	var url_ridi = 'http://ridibooks.com/search/?q='+queryName;
-	var url_usb = 'http://userstorybook.net/books/?keyword='+queryName;
+
+	function urlEncode(str){
+	    str=escape(str);
+	    str=str.replace(new RegExp('\\+','g'),'%2B');
+	    return str.replace(new RegExp('%20','g'),'+');
+	}
+	var queryname = document.getElementById('inp').value;
+	var querynameurl = urlEncode(queryname);
+
+	var url_aladin = 'http://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord='+querynameurl;
+	var url_ridi = 'http://ridibooks.com/search/?q='+queryname;
+	var url_usb = 'http://userstorybook.net/books/?keyword='+queryname;
+	window.open(url_aladin);
 	window.open(url_ridi);
 	window.open(url_usb);
 	document.getElementsByTagName('body')[0].removeChild(frame);
@@ -47,9 +57,8 @@ button_close.addEventListener('click', function(){
 document.getElementsByTagName('body')[0].appendChild(frame);
 
 /*
-SINGLE LINE CONVERTING RESULT 
-using http://mrcoles.com/bookmarklet/
+function urlEncode() : from : https://ostermiller.org/calc/encode.html
 
-javascript:(function()%7Bvar%20frame%20%3D%20document.createElement('div')%3Bframe.style.zIndex%20%3D%20999999%3Bframe.style.top%20%3D%20'0px'%3Bframe.style.right%20%3D%20'0px'%3Bframe.style.position%20%3D%20'fixed'%3Bframe.style.width%20%3D%20'400px'%3Bframe.style.padding%20%3D%20'10px'%3Bframe.style.background%20%3D%20'%23f0f0f0'%3Bvar%20guide%20%3D%20document.createElement('p')%3Bguide.innerHTML%20%3D%20'%EC%A0%9C%EB%AA%A9%EC%9D%B4%EB%82%98%20%EC%A0%80%EC%9E%90'%3Bguide.style.margin%20%3D'0%206px%200%200'%3Bvar%20input_box%20%3D%20document.createElement('input')%3Binput_box.setAttribute('type'%2C'text')%3Binput_box.setAttribute('size'%2C'15')%3Binput_box.id%20%3D%20'inp'%3Binput_box.style.margin%20%3D%20'0%206px%200%200'%3Bvar%20button_find%20%3D%20document.createElement('input')%3Bbutton_find.setAttribute('type'%2C'button')%3Bbutton_find.setAttribute('value'%2C'%EC%B0%BE%EC%95%84%EC%A4%98')%3Bvar%20button_close%20%3D%20document.createElement('input')%3Bbutton_close.setAttribute('type'%2C'button')%3Bbutton_close.setAttribute('value'%2C'%EB%8B%AB%EA%B8%B0')%3Bframe.appendChild(guide)%3Bframe.appendChild(input_box)%3Bframe.appendChild(button_find)%3Bframe.appendChild(button_close)%3Bbutton_find.addEventListener('click'%2C%20function()%7Bvar%20queryName%20%3D%20document.getElementById('inp').value%3Bvar%20url_ridi%20%3D%20'http%3A%2F%2Fridibooks.com%2Fsearch%2F%3Fq%3D'%2BqueryName%3Bvar%20url_usb%20%3D%20'http%3A%2F%2Fuserstorybook.net%2Fbooks%2F%3Fkeyword%3D'%2BqueryName%3Bwindow.open(url_ridi)%3Bwindow.open(url_usb)%3Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bbutton_close.addEventListener('click'%2C%20function()%7Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bdocument.getElementsByTagName('body')%5B0%5D.appendChild(frame)%7D)()
-
+SINGLE LINE CONVERTING RESULT : from : http://mrcoles.com/bookmarklet
+javascript:(function()%7Bvar%20frame%20%3D%20document.createElement('div')%3Bframe.style.zIndex%20%3D%20999999%3Bframe.style.top%20%3D%20'0px'%3Bframe.style.right%20%3D%20'0px'%3Bframe.style.position%20%3D%20'fixed'%3Bframe.style.width%20%3D%20'400px'%3Bframe.style.padding%20%3D%20'10px'%3Bframe.style.background%20%3D%20'%23f0f0f0'%3Bvar%20guide%20%3D%20document.createElement('p')%3Bguide.innerHTML%20%3D%20'%EC%A0%9C%EB%AA%A9%EC%9D%B4%EB%82%98%20%EC%A0%80%EC%9E%90'%3Bguide.style.margin%20%3D'0%206px%200%200'%3Bvar%20input_box%20%3D%20document.createElement('input')%3Binput_box.setAttribute('type'%2C'text')%3Binput_box.setAttribute('size'%2C'15')%3Binput_box.id%20%3D%20'inp'%3Binput_box.style.margin%20%3D%20'0%206px%200%200'%3Bvar%20button_find%20%3D%20document.createElement('input')%3Bbutton_find.setAttribute('type'%2C'button')%3Bbutton_find.setAttribute('value'%2C'%EC%B0%BE%EC%95%84%EC%A4%98')%3Bvar%20button_close%20%3D%20document.createElement('input')%3Bbutton_close.setAttribute('type'%2C'button')%3Bbutton_close.setAttribute('value'%2C'%EB%8B%AB%EA%B8%B0')%3Bframe.appendChild(guide)%3Bframe.appendChild(input_box)%3Bframe.appendChild(button_find)%3Bframe.appendChild(button_close)%3Bbutton_find.addEventListener('click'%2C%20function()%7Bfunction%20urlEncode(str)%7Bstr%3Descape(str)%3Bstr%3Dstr.replace(new%20RegExp('%5C%5C%2B'%2C'g')%2C'%252B')%3Breturn%20str.replace(new%20RegExp('%2520'%2C'g')%2C'%2B')%3B%7Dvar%20queryname%20%3D%20document.getElementById('inp').value%3Bvar%20querynameurl%20%3D%20urlEncode(queryname)%3Bvar%20url_aladin%20%3D%20'http%3A%2F%2Fwww.aladin.co.kr%2Fsearch%2Fwsearchresult.aspx%3FSearchTarget%3DAll%26SearchWord%3D'%2Bquerynameurl%3Bvar%20url_ridi%20%3D%20'http%3A%2F%2Fridibooks.com%2Fsearch%2F%3Fq%3D'%2Bqueryname%3Bvar%20url_usb%20%3D%20'http%3A%2F%2Fuserstorybook.net%2Fbooks%2F%3Fkeyword%3D'%2Bqueryname%3Bwindow.open(url_aladin)%3Bwindow.open(url_ridi)%3Bwindow.open(url_usb)%3Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bbutton_close.addEventListener('click'%2C%20function()%7Bdocument.getElementsByTagName('body')%5B0%5D.removeChild(frame)%3B%7D)%3Bdocument.getElementsByTagName('body')%5B0%5D.appendChild(frame)%7D)()
 */
